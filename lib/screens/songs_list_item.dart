eca0f8a9491a470c7ac64d1d7f305327cc03e1d7
Federico Martinetti <frmmartinetti98@gmail.com>
2025-02-03 22:14:41 -0300

    screen para canciones individuales actualizada

--- lib/screens/songs_list_item.dart
+++ lib/screens/songs_list_item.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_base/widgets/drawer_menu.dart';
+import '../widgets/animated_play_button.dart';
+import '../widgets/glassmorphism.dart';
 
 class SongsListItem extends StatefulWidget {
   const SongsListItem({ super.key });
-
   @override
   State<SongsListItem> createState() => _SongsListItemState();
 }
@@ -11,17 +13,25 @@ class _SongsListItemState extends State<SongsListItem> {
   double currentTime = 0;
   double volume = 0.5;
   bool isPlaying = false;
-  Map<String, dynamic>? currentSong;
-  
+  bool isLoading = true;
+  late Map<String, dynamic> currentSong = {};
+  late List<Map<String, dynamic>> songs;
+  late int currentIndex;
+
   @override
-  void initState() {
-    super.initState();
-    currentSong = {
-      'song': 'song',
-      'artist': 'artist',
-      'album': 'album',
-      'cover': 'cover',
-    };
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final args = ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>;
+    songs = List<Map<String, dynamic>>.from(args['songs_list']);
+    currentIndex = args['index'];
+    currentSong = songs[currentIndex];
+    isLoading = false;
+  }
+  
+  void _toggleFavorite() {
+    setState(() {
+      currentSong['isFavorite'] = !currentSong['isFavorite'];
+    });
   }
 
   void playPause(){
@@ -31,163 +41,225 @@ class _SongsListItemState extends State<SongsListItem> {
   }
 
   void nextSong() {
-    setState(() {
-      currentSong = {
-        'song': 'song',
-        'artist': 'artist',
-        'album': 'album',
-        'cover': 'cover',
-      };
-      currentTime = 0;
-      isPlaying = true;
-    });
+    if (currentIndex < songs.length - 1) {
+      setState(() {
+        currentIndex++;
+        currentSong = songs[currentIndex];
+        currentTime = 0;
+        isPlaying = true;
+        isLoading = false;
+      });
+    }
   }
 
   void previousSong() {
-    setState(() {
-      currentSong = {
-        'song': 'song',
-        'artist': 'artist',
-        'album': 'album',
-        'cover': 'cover',
-      };
-      currentTime = 0;
-      isPlaying = true;
-    });
+    if (currentIndex > 0) {
+      setState(() {
+        currentIndex--;
+        currentSong = songs[currentIndex];
+        currentTime = 0;
+        isPlaying = true;
+        isLoading = false;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // Obtiene los argumentos pasados
-    final args = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
-    // Accede a cada argumento con su clave
-    final size = MediaQuery.of(context).size;
+    final bool isDarkTheme = Theme.of(context).brightness == Brightness.dark;
+    if (isLoading) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
 
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('songs'),
-        titleTextStyle: const TextStyle(fontFamily: 'Poppins', fontSize: 26, fontWeight: FontWeight.bold),
-        elevation: 10,
-        backgroundColor: const Color.fromARGB(255, 150, 16, 216),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            HeaderProfileCustomItem(
-              size: size,
-              songCover: args['songCover'],
-            ),
-            Padding(
-              padding: const EdgeInsets.all(15),
-              child: BodyProfileCustomItem(args: args),
-            ),
-          const SizedBox(height: 10),
-          Column(
-            children: [
-              Slider(
-                activeColor: const Color.fromARGB(255, 118, 17, 206),
-                value: currentTime,
-                max: 100,
-                onChanged: (value) {
-                  setState(() {
-                    currentTime = value;
-                  });
-                }
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.skip_previous_rounded, size: 35),
-                    onPressed: previousSong,
+  return Scaffold(
+    appBar: AppBar(toolbarHeight: 65), backgroundColor: isDarkTheme ? Colors.black : Colors.white,
+    endDrawer: const DrawerMenu(),
+    body: Column(
+      children: [
+          // Imagen superior
+          Expanded(
+            flex: 6,
+            child: Stack(
+              children: [
+                Positioned.fill(
+                  child: Image.network(
+                    currentSong['image']!,
+                    fit: BoxFit.cover, // Ajusta la imagen para cubrir todo el área disponible
                   ),
-                  IconButton(
-                    icon: Icon(isPlaying ? Icons.pause_rounded : Icons.play_arrow_rounded, size: 50),
-                    onPressed: playPause,
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.skip_next_rounded, size: 35),
-                    onPressed: nextSong,
+                ),
+                Positioned(
+                  top: 0,
+                  child: Container(
+                    height: 80,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          isDarkTheme ? const Color.fromARGB(255, 12, 10, 15) : Colors.transparent,
+                          Colors.transparent,
+                        ],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                      )
+                    )
                   )
-                ] 
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Padding(padding: EdgeInsets.only(left: 20), child: Icon(Icons.volume_down_rounded)),
-                  Expanded(
-                    child: Slider(
-                      activeColor: const Color.fromARGB(208, 70, 113, 231),
-                      value: volume,
-                      max: 1,
-                      onChanged: (value) {
-                        setState(() {
-                          volume = value;
-                        });
-                      }
+                ),
+                // Gradiente superpuesto
+                Positioned(
+                  bottom: -1,
+                  child: Container(
+                    height: 150, // Altura del gradiente ajustada
+                    width: MediaQuery.of(context).size.width, // Ancho completo
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          isDarkTheme ? const Color.fromARGB(255, 12, 10, 15) : Colors.transparent,
+                          isDarkTheme ? const Color.fromARGB(255, 12, 10, 15).withValues(alpha: 0.8) : Colors.transparent,
+                          Colors.transparent,
+                        ],
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                      ),
                     ),
                   ),
-                  const Padding(padding: EdgeInsets.only(right: 20), child: Icon(Icons.volume_up_rounded))
-                  ]
+                ),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 150,
+                    padding: const EdgeInsets.all(10),
+                    child: Glassmorphism(blur: 10, opacity: 0.1, radius: 20,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  padding: const EdgeInsets.all(10),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(currentSong['name'] ?? 'Not found',style: const TextStyle(fontFamily: 'Poppins', fontSize: 22, fontWeight: FontWeight.bold, color: Color.fromARGB(203, 255, 255, 255)), overflow: TextOverflow.ellipsis, maxLines: 1),
+                                      const SizedBox(height: 10),
+                                      Text(currentSong['artist'] ?? 'Not found', style: const TextStyle(fontSize: 20, color: Color.fromARGB(203, 255, 255, 255)), overflow: TextOverflow.ellipsis, maxLines: 1),
+                                      const SizedBox(height: 10),
+                                    ]
+                                  )
+                                )
+                              ),                       
+                              IconButton(
+                                icon: currentSong['isFavorite'] ?
+                                  ShaderMask(
+                                  shaderCallback: (bounds) => const LinearGradient(
+                                    colors: [Color.fromARGB(255, 109, 37, 233), Color.fromARGB(255, 79, 130, 239)],
+                                    begin: Alignment.centerLeft,
+                                    end: Alignment.centerRight,
+                                  ).createShader(bounds),
+                                  child: const Icon(Icons.favorite_sharp, color: Colors.white),
+                                ) : const Icon(Icons.favorite, color: Color.fromARGB(133, 171, 171, 171)),
+                                onPressed: _toggleFavorite
+                              )
+                            ]
+                          ),
+                        ]
+                      )
+                    )
                 )
               ],
-            )
-          ]
-        )  
+            ),
+          ),
+          // Contenido principal
+          Expanded(
+            flex: 3,
+            child: Container(
+              color: isDarkTheme ? const Color.fromARGB(255, 12, 10, 15) : Colors.white,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Column(
+                    children: [
+                      Slider(
+                        activeColor: const Color.fromARGB(255, 118, 17, 206),
+                        value: currentTime,
+                        max: 100,
+                        onChanged: (value) {
+                          setState(() {
+                            currentTime = value;
+                          });
+                        }
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.shuffle),
+                            onPressed: previousSong,
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.skip_previous_rounded, size: 35),
+                            onPressed: previousSong,
+                          ),
+                          AnimatedPlayButton(
+                            isPlaying: isPlaying,
+                            onPressed: playPause,
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.skip_next_rounded, size: 35),
+                            onPressed: nextSong,
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.repeat_rounded),
+                            onPressed: nextSong,
+                          )
+                        ] 
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Padding(padding: EdgeInsets.only(left: 20), child: Icon(Icons.volume_down_rounded)),
+                          Expanded(
+                            child: Slider(
+                              activeColor: const Color.fromARGB(208, 70, 113, 231),
+                              value: volume,
+                              max: 1,
+                              onChanged: (value) {
+                                setState(() {
+                                  volume = value;
+                                });
+                              }
+                            ),
+                          ),
+                          const Padding(padding: EdgeInsets.only(right: 20), child: Icon(Icons.volume_up_rounded))
+                          ]
+                        ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Image.asset('assets/images/gradient.png', fit: BoxFit.cover),
+          )
+        ],
       ),
-    );
-  }
-}
-
-class BodyProfileCustomItem extends StatelessWidget {
-  final Map<String, dynamic> args;
-
-  const BodyProfileCustomItem({super.key, required this.args});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          args['song'],
-          style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(height: 10),
-        Text(
-          args['artist'],
-          style: const TextStyle(fontSize: 20),
-          textAlign: TextAlign.center,
-        ),
-        const SizedBox(height: 10),
-        Text(
-          args['album'], 
-          style: const TextStyle(fontSize: 18)
-        )
-      ],
-    );
-  }
-
-  InputDecoration decorationInput(
-      {IconData? icon, String? hintText, String? helperText, String? label}) {
-    return InputDecoration(
-      fillColor: Colors.black,
-      label: Text(label ?? ''),
-      hintText: hintText,
-      helperText: helperText,
-      helperStyle: const TextStyle(fontSize: 16),
-      prefixIcon: (icon != null) ? Icon(icon) : null,
     );
   }
 }
 
 class HeaderProfileCustomItem extends StatelessWidget {
   final Size size;
-  final String? songCover;
+  final String songCover;
 
   const HeaderProfileCustomItem({
     super.key,
-    this.songCover,
+    required this.songCover,
     required this.size,
   });
 
@@ -200,8 +272,8 @@ class HeaderProfileCustomItem extends StatelessWidget {
       child: Center(
         child: ClipRRect(
           borderRadius: BorderRadius.circular(20),
-          child: songCover != ""
-              ? Image.asset('assets/songs/$songCover.jpg', fit: BoxFit.cover)
+          child: songCover.isNotEmpty
+              ? Image.network(songCover, fit: BoxFit.cover)
               : Image.asset('assets/images/album.png', fit: BoxFit.cover),
         ),
       ),

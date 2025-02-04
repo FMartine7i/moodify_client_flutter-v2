8253321cbec923d4f55975aa4aad2f23c3feb3e2
Federico Martinetti <frmmartinetti98@gmail.com>
2025-02-03 22:15:25 -0300

    cambios en rutas de screens

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -3,4 +3,5 @@ export 'package:flutter_application_base/screens/songs_list_item.dart';
 export 'package:flutter_application_base/screens/profile_screen.dart';
 export 'package:flutter_application_base/screens/songs_list_screen.dart';
 export 'package:flutter_application_base/screens/home_screen.dart';
-export 'package:flutter_application_base/screens/albumes_list_screen.dart';
+export 'package:flutter_application_base/screens/albums_list_screen.dart';
+export 'package:flutter_application_base/screens/album_item_screen.dart';
